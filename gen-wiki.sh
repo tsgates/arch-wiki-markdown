81ba497110f2c13c5ee6f0509b6ca420210c9227
Kevin MacMartin <prurigro@gmail.com>
2014-07-13 18:54:50 -0400

    Did some minor cleanup to the bash

--- gen-wiki.sh
+++ gen-wiki.sh
@@ -1,17 +1,17 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 DIR=$(dirname "$0")
 URL=$(pacman -Sp arch-wiki-docs)
 OUT=${1:-$(mktemp -d)}
-ZIP=$OUT/arch-wiki-docs.tar.xz
+ZIP=${OUT}/arch-wiki-docs.tar.xz
 
 (cd $DIR && {
     if [[ ! -e $ZIP ]]; then
-      wget $URL -O $ZIP
-      mkdir $OUT/pkg
-      uz -X $OUT/pkg $ZIP
+        wget $URL -O $ZIP
+        mkdir ${OUT}/pkg
+        uz -X ${OUT}/pkg $ZIP
     fi
     ghc filter.hs +RTS -N4
-    ./filter $OUT/pkg
+    ./filter ${OUT}/pkg
     pacman -Si arch-wiki-docs > wiki/version
 })
