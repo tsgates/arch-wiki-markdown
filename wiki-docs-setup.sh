7b73a778d49c2656b56f20805389b62d97f3d5ff
Kevin MacMartin <prurigro@gmail.com>
2014-07-14 14:46:06 -0400

    Usign a temp directory due to a name conflict

--- wiki-docs-setup.sh
+++ wiki-docs-setup.sh
@@ -3,7 +3,12 @@
 WIKIDOCS="src/wiki-docs"
 
 [[ -d "$WIKIDOCS" ]] && echo "The ${WIKIDOCS} directory already exists" && exit 1
-[[ -d "src" ]] || install -d src
-curl $(pacman -Sp arch-wiki-docs) | tar -xJ -C src/ >/dev/null 2>&1
-mv src/arch-wiki-docs "$WIKIDOCS"
-pacman -Si arch-wiki-docs | grep -e "^Version" | sed 's|^[^:]*: ||;s|-1$||' > "${WIKIDOCS}/date"
+
+[[ -d "tmp" ]] && rm -rf tmp
+install -d tmp
+curl $(pacman -Sp arch-wiki-docs) | tar -xJ -C tmp/ >/dev/null 2>&1
+[[ -d src ]] || install -d src
+mv tmp "$WIKIDOCS"
+rm -rf tmp
+
+(pacman -Si arch-wiki-docs | grep -e "^Version" | sed 's|^[^:]*: ||;s|-1$||') > "${WIKIDOCS}/date"
