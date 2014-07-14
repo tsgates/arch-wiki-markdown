30b4fc17cce1e28eda69782dd442b1bf7b1a194d
Kevin MacMartin <prurigro@gmail.com>
2014-07-14 16:26:02 -0400

    removing arch-wiki-docs-setup.sh as wiki-docs.setup.sh replaces it

--- arch-wiki-docs-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-WIKIDOCS="src/wiki-docs"
-
-[[ -d "$WIKIDOCS" ]] && echo "The ${WIKIDOCS} directory already exists" && exit 1
-install -d "$WIKIDOCS"
-curl $(pacman -Sp arch-wiki-docs) | tar -xJ -C "${WIKIDOCS}/" >/dev/null 2>&1
-pacman -Si arch-wiki-docs | grep -e "^Version" | sed 's|^[^:]*: ||;s|-1$||' > "${WIKIDOCS}/date"
