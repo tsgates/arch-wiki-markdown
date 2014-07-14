725f48272e83541393839829f47840ed658d4d37
Kevin MacMartin <prurigro@gmail.com>
2014-07-14 14:31:33 -0400

    Fixed a bug and setting the tar output to silent

--- arch-wiki-docs-setup.sh
+++ arch-wiki-docs-setup.sh
@@ -4,5 +4,5 @@ WIKIDOCS="src/wiki-docs"
 
 [[ -d "$WIKIDOCS" ]] && echo "The ${WIKIDOCS} directory already exists" && exit 1
 install -d "$WIKIDOCS"
-curl $(pacman -Sp arch-wiki-docs) | tar -xJ -C "${WIKIDOCS}/"
-pacman -Si arch-wiki-docs | grep -e "^Version" | sed 's|^[^:]*: ||;s|-1$||' > "${WIKIDOCS}/date
+curl $(pacman -Sp arch-wiki-docs) | tar -xJ -C "${WIKIDOCS}/" >/dev/null 2>&1
+pacman -Si arch-wiki-docs | grep -e "^Version" | sed 's|^[^:]*: ||;s|-1$||' > "${WIKIDOCS}/date"
