3b4c16ed568db93347e8f9cfcd399f832f023619
Kevin MacMartin <prurigro@gmail.com>
2014-07-14 22:20:34 -0400

    Tweaked the wiki-docs setup script to work in more scenarios

--- wiki-docs-setup.sh
+++ wiki-docs-setup.sh
@@ -3,12 +3,13 @@
 WIKIDOCS="src/wiki-docs"
 
 [[ -d "$WIKIDOCS" ]] && echo "The ${WIKIDOCS} directory already exists" && exit 1
+install -d "$WIKIDOCS"
+
+(pacman -Si arch-wiki-docs | grep -e "^Version" | sed 's|^[^:]*: ||;s|-1$||') > "${WIKIDOCS}/date"
 
 [[ -d "tmp" ]] && rm -rf tmp
 install -d tmp
-curl $(pacman -Sp arch-wiki-docs) | tar -xJ -C tmp/ >/dev/null 2>&1
-[[ -d src ]] || install -d src
-mv tmp/usr/share/doc/arch-wiki/html "$WIKIDOCS"
-rm -rf tmp
 
-(pacman -Si arch-wiki-docs | grep -e "^Version" | sed 's|^[^:]*: ||;s|-1$||') > "${WIKIDOCS}/date"
+curl $(pacman -Sp arch-wiki-docs) | tar -xJ -C tmp/ >/dev/null 2>&1
+mv tmp/usr/share/doc/arch-wiki/html/* "${WIKIDOCS}/"
+rm -rf tmp
